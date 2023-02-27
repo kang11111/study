d7431c5398ecdd3938023dfc094e40c241ff40e4
Ahyoung Kang <dkduddl1262@gmail.com>
2023-02-27 18:05:00 +0900

    점수, 콤보, 시간초과 기능 추가

--- imageMatching_game/script.js
+++ imageMatching_game/script.js
@@ -1,16 +1,22 @@
-//도형모양에 따라 1 또는 2 번호가 부여되어있고 순서에 따라 또 번호가 있음
-//사용자 버튼 클릭 1 또는 2
-//사용자가 누른 버튼 == 도형모양번호
-//눌린 도형 사라지고 그 다음 도형이 앞으로 와서 0번 번호 부여받기
-
 const imageContainer = document.getElementById('image_container')
 
 let imageArr = new Array(); //랜덤으로 지정된 도형 모양에 따른 번호를 저장하는 배열
 let image = '';
 let imageNum;
 let userBtn = '';
-// let userArr = new Array(); //사용자가 누른 버튼 번호를 저장하는 배열
-const lastNum = 4; //노출되는 도형 개수
+const lastNum = 7; //노출되는 도형 개수
+const arrowLeft = document.querySelector("#arrow_container button:nth-child(1)");
+const arrowRight = document.querySelector("#arrow_container button:nth-child(3)");
+let score = 0;
+let combo = 0;
+const scoreContainer = document.getElementById('score');
+const comboContainer = document.getElementById('combo');
+let time = 30;
+const timeContainer = document.getElementById('time');
+let totalScore = 0;
+const gameOverContainer = document.getElementById('gameOver');
+const totalScoreContainer = document.querySelector('#gameOver > p:nth-child(2)');
+const resetButton = document.querySelector('#gameOver > button');
 
 //imageNum = Math.floor((Math.random() * 3+1)) //도형 번호를 랜덤으로 1~3까지 추출
 
@@ -31,16 +37,37 @@ for(let i=0;i<lastNum;i++){ //도형 4개 생성
 imageContainer.innerHTML = image;
 
 window.addEventListener("keydown", (e) => { //화살표 버튼을 눌렀을 때 사용자가 누른 버튼 번호 저장
-  if(e.code == 'ArrowLeft'){
-    userBtn = 1;
-  }else if(e.code == 'ArrowRight'){
-    userBtn = 2;
+  if(time == 0){ //시간초과가 되면 키보드 안 눌리게 함
+    e.preventDefault();
+  }else{
+    if(e.code == 'ArrowLeft'){
+      userBtn = 1;
+    }else if(e.code == 'ArrowRight'){
+      userBtn = 2;
+    }
   }
-  console.log(imageArr)
-  console.log(imageArr[0])
-  console.log(userBtn)
+  matching();
+});
+
+arrowLeft.addEventListener("click", ()=>{
+  userBtn = 1;
+  matching();
+})
+arrowRight.addEventListener("click", ()=>{
+  userBtn = 2;
+  matching();
+})
+
+resetButton.addEventListener("click", ()=>{ //다시 시작
+  location.reload();
+})
+
+function matching() {
   if(imageArr[0] == userBtn){ //도형배열 0번와 사용자입력번호가 일치하면
-    console.log('버튼과 배열 내용이 일치');
+    score += 10; //점수 10점 추가
+    combo += 1; //콤보 1점 추가
+    scoreContainer.innerHTML = `${score}점`
+    comboContainer.innerHTML = `${combo}콤보`
     imageArr.shift(); //도형배열 0번 삭제
     imageNum = Math.floor((Math.random() * 2+1)); //이미지번호 1개를 랜덤으로 추출
     imageArr.push(imageNum); //도형번호배열에 랜덤으로 추출한 1개 이미지 번호를 넣기
@@ -55,5 +82,23 @@ window.addEventListener("keydown", (e) => { //화살표 버튼을 눌렀을 때 
       image += `<img class="img" src="./img/image${imageArr[j]}.png" alt="">`;
     }
     imageContainer.innerHTML = image;
+  }else if(imageArr[0] != userBtn){ //일치하지않으면
+    combo = 0;
+    comboContainer.innerHTML = `${combo}콤보`
   }
-});
+}//fn end
+
+let timeInterval = setInterval(() => { //1초가 지날 때마다 실행됨
+  time -= 1;
+  timeContainer.innerHTML = `${time}초`;
+  if(time == 0){ //0초가 되면
+    clearInterval(timeInterval);
+    totalScore = score + combo * 100; //총점 계산
+    arrowLeft.disabled = true; //화살표 버튼 눌러도 안 눌리게 함
+    arrowRight.disabled = true;
+    arrowLeft.style.cursor = "default"; //커서 모양 변경
+    arrowRight.style.cursor = "default";
+    gameOverContainer.style.display = "block";
+    totalScoreContainer.innerHTML = `총점 : ${totalScore}`;
+  }
+}, 1000);
